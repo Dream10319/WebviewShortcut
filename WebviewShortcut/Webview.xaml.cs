6a9a12159efa33f080e5678b600d0e575f3fb54f
Vigil1Light <zerry319.wit@gmail.com>
2024-05-19 04:20:32 +0800

    cookie issue

--- WebviewShortcut/Webview.xaml.cs
+++ WebviewShortcut/Webview.xaml.cs
@@ -24,10 +24,7 @@ namespace WebviewShortcut
             image.EndInit();
             ImageBehavior.SetAnimatedSource(loadingImage, image);
 
-
-
             Browser.Address = url;
-            //Browser.FrameLoadEnd += OnFrameLoadEnd;
             
             Dispatcher.InvokeAsync(async () =>
             {
@@ -52,37 +49,10 @@ namespace WebviewShortcut
             });
         }
 
-        private void OnFrameLoadEnd(object sender, FrameLoadEndEventArgs e)
-        {
-            ChromiumWebBrowser browser = (ChromiumWebBrowser)sender;
-            if (e.Frame.IsMain)
-            {
-                // Delay the script execution to ensure the page is fully loaded
-                Dispatcher.InvokeAsync(async () =>
-                {
-                    await Task.Delay(1000); // Delay to ensure all elements are loaded
-
-                    // JavaScript to fill the form and click the login button
-                    string script = "";
-                    if (browser.Address == "https://biz-member.baemin.com/login")
-                    {
-                        script = @"
-                        document.querySelector('input[data-testid=""id""]').value = 'Yang8034';
-                        document.querySelector('input[data-testid=""password""]').value = 'Yhs1137@';
-                        document.querySelector('button.Button__StyledButton-sc-1cxc4dz-0.hlLPsc').click();
-                        window.location.href = 'https://self.baemin.com/';
-                    ";
-                    }
-
-                    // Execute the script
-                    Browser.ExecuteScriptAsync(script);
-                });
-            }
-        }
-
         protected override void OnClosed(EventArgs e)
         {
             Browser.Dispose();
+            Cef.GetGlobalCookieManager().DeleteCookies("", "");
             base.OnClosed(e);
         }
     }
